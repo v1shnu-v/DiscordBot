33df79925b71951d9d8ce801df0b786a96ad7baa
Vishnu Vijay <itsvishnu123@gmail.com>
2021-05-02 02:47:36 +0530

    Updated Image.paste() with Image.alpha_composite
    This was done so that the user's circular pfp wouldn't loose its transparency while being pasted on the background. Also Image.ANTIALIAS was done because of which the edges are much smoother now. 
    
    The images used in Image.alpha_composite has to be RGBA so both images were converted to RGBA.
    
    Exact position of the images need to be changed and fixed so as to fit the background.

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ client = discord.Client(intents = intents)
 
 @client.event
 async def on_ready():
-    print("Bot is ready")       #Prints "Bot is ready" in the console when it is deployed 
+    print("Bot is ready for action")       #Prints "Bot is ready" in the console when it is deployed 
 
 
 
@@ -27,56 +27,40 @@ async def on_member_join(member):
     #IDs
     #server = client.get_guild(433618651074134028)                          #server id
     welcome_channel = member.guild.get_channel(837777546643898399)          #channel id - edit this to the ID of the welcome channel
-    
+
+#------------------------------------------------------------------------------------------------------------------------    
     #embed
-    myEmbed = discord.Embed(title="This is the title", description="This is the description", color=0x00ff00)
-    myEmbed.add_field(name="This is filed1", value="This is value1", inline=False)
-    myEmbed.add_field(name="This is field2", value="This is value2", inline=True)
-    myEmbed.set_footer(text="This is a sample footer")
-    myEmbed.set_author(name="Author Name")
+    #myEmbed = discord.Embed(title="This is the title", description="This is the description", color=0x00ff00)
+    #myEmbed.add_field(name="This is filed1", value="This is value1", inline=False)
+    #myEmbed.add_field(name="This is field2", value="This is value2", inline=True)
+    #myEmbed.set_footer(text="This is a sample footer")
+    #myEmbed.set_author(name="Author Name")
 
-    await welcome_channel.send(embed=myEmbed)
+    #await welcome_channel.send(embed=myEmbed)
 
-
+#-------------------------------------------------------------------------------------------------------------------------
 
     #test image
     #await welcome_channel.send(file=discord.File('image.png'))
 
 
     #image manipulation
-    
-    img = Image.open("image.png")       #background image
 
-    asset = member.avatar_url_as(size = 128)  #asset is member pfp
-    data = BytesIO(await asset.read())
-
-    pfp= Image.open(data)
-               
-
-#----------------------------------------------------------
-    blur_radius = 0
-    offset = 0
-    offset = blur_radius * 2 + offset
-    mask = Image.new("L", pfp.size, 0)
-    draw = ImageDraw.Draw(mask)
-    draw.ellipse((offset, offset, pfp.size[0] - offset, pfp.size[1] - offset), fill=255)
-    mask = mask.filter(ImageFilter.GaussianBlur(blur_radius))
-
-    result = pfp.copy()
-    result.putalpha(mask)
-    result.save('pfp_round.png')            #save round image
-    await welcome_channel.send(file=discord.File('pfp_round.png'))  #send round pfp image
-    
-#-----------------------------------------------------------
-    rndpfp = Image.open('pfp_round.png')
-    rndpfp = rndpfp.resize((250,250))
-
-    img.paste(rndpfp, (350,150))          #paste pfp on background
-    img.save('combined.png')
-
-    
-
-    await welcome_channel.send(file=discord.File('combined.png'))  #send round pfp image
+    bg = Image.open("bg.png").convert("RGBA")
+    im = Image.open(BytesIO(await member.avatar_url_as(size=256).read())).convert("RGBA")
+    overlay = Image.new('RGBA', bg.size, (255,255,255,0))
+    masksize = (im.size[0] * 3, im.size[1] * 3)
+    mask = Image.new('L', masksize, 0)
+    draw = ImageDraw.Draw(mask) 
+    draw.ellipse((0, 0) + masksize, fill=255)
+    mask = mask.resize(im.size, Image.ANTIALIAS)
+    im.putalpha(mask)
+    overlay.paste(im, (425, 58))
+    compose = Image.alpha_composite(bg, overlay)
+    arr = BytesIO()
+    compose.save(arr, format='png')
+    arr.seek(0)
+    await welcome_channel.send(file = discord.File(arr, 'profile.png'))
     
     
 
