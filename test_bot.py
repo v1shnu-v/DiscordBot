3ef177753f6a7d0e7df6c452f8c45fb52a3edbba
Vishnu Vijay <itsvishnu123@gmail.com>
2021-05-02 18:47:43 +0530

    Welcome module complete

--- test_bot.py
+++ test_bot.py
@@ -10,7 +10,7 @@ token = "ODM3OTYwNDczMzU2Nzk1OTA0.YI0JgQ.vQjqzwJtB-4GNQY38iC6TR9AxRw"
 
 intents = discord.Intents.default()
 intents.members = True
-client = discord.Client(intents = intents)
+client = commands.Bot(command_prefix = '!',intents = intents)
 
 
 
@@ -20,28 +20,21 @@ async def on_ready():
 
 
 
-#main function
+#welcome function
 @client.event
 async def on_member_join(member):
     
     #IDs
-    #server = client.get_guild(433618651074134028)                          #server id
+    server = client.get_guild(433618651074134028)                           #server id
     welcome_channel = member.guild.get_channel(837777546643898399)          #channel id - edit this to the ID of the welcome channel
 
-#------------------------------------------------------------------------------------------------------------------------    
-    #embed
-    #myEmbed = discord.Embed(title="This is the title", description="This is the description", color=0x00ff00)
-    #myEmbed.add_field(name="This is filed1", value="This is value1", inline=False)
-    #myEmbed.add_field(name="This is field2", value="This is value2", inline=True)
-    #myEmbed.set_footer(text="This is a sample footer")
-    #myEmbed.set_author(name="Author Name")
 
-    #await welcome_channel.send(embed=myEmbed)
-
-#-------------------------------------------------------------------------------------------------------------------------
-
-    print(f'{member} has joined the server')
+    print(f'{member} has joined the server')    #for logs
+    
     await welcome_channel.send(f'Keri vaada {member.mention}! Welcome to **Gamers Hub**!')
+    
+    
+    print(f'Members = {server.member_count}')
 
     
 
@@ -58,13 +51,16 @@ async def on_member_join(member):
     avatar.putalpha(mask)
     overlay.paste(avatar, (422, 61))
     compose = Image.alpha_composite(bg, overlay)
-#-----------------------------------------------------------------
+
+    
+    #font for welcome banner
     font = ImageFont.truetype('arial.ttf', size=38)
     draw = ImageDraw.Draw(compose)
     
+    #message on welcome banner
+    message = f'{member} has joined the server \nGamer #{server.member_count}'
 
-    message = f'{member} has joined the server'
-
+    #box to bound welcome banner text
     bounding_box = [53, 333, 1048, 420]
     x1, y1, x2, y2 = bounding_box  # For easy reading
 
@@ -83,11 +79,12 @@ async def on_member_join(member):
     #Draw the bounding box which bounds the text
     #draw.rectangle([x1, y1, x2, y2])
 
-#------------------------------------------------------------------------------------------    
+   
     arr = BytesIO()
     compose.save(arr, format='png')
     arr.seek(0)
     await welcome_channel.send(file = discord.File(arr, 'profile.png'))
+
     
     
 
@@ -96,3 +93,18 @@ async def on_member_join(member):
     
 
 client.run(token)
+
+
+
+
+#------------------------------------------------------------------------------------------------------------------------    
+    #embed
+    #myEmbed = discord.Embed(title="This is the title", description="This is the description", color=0x00ff00)
+    #myEmbed.add_field(name="This is filed1", value="This is value1", inline=False)
+    #myEmbed.add_field(name="This is field2", value="This is value2", inline=True)
+    #myEmbed.set_footer(text="This is a sample footer")
+    #myEmbed.set_author(name="Author Name")
+
+    #await welcome_channel.send(embed=myEmbed)
+
+#-------------------------------------------------------------------------------------------------------------------------
