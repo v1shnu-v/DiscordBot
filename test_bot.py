260f29ce1edbb06b2bce1da21150bd070706cbf5
Vishnu Vijay <itsvishnu123@gmail.com>
2021-05-02 21:02:59 +0530

    token = read_token()
    Stored the server token in token.txt. Now token reads token.txt to get the value of server token

--- test_bot.py
+++ test_bot.py
@@ -4,8 +4,14 @@ from discord.ext import commands
 from PIL import Image, ImageOps, ImageDraw, ImageFont   #Pillow Library for image manipulations
 from io import BytesIO
 
-token = "ODM3OTYwNDczMzU2Nzk1OTA0.YI0JgQ.vQjqzwJtB-4GNQY38iC6TR9AxRw"
 
+def read_token():
+    with open("token.txt","r") as f:
+        lines = f.readlines()
+        return lines[0].strip()
+
+
+token = read_token()    
 
 
 intents = discord.Intents.default()
